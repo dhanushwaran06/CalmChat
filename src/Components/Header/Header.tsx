09020950864870539011ede776a088d94c89ae49
RyanXGregoire <ryangregoire286@gmail.com>
2024-07-07 21:56:39 -0400

    Added Browser Routing from App

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,7 @@ import "../Styles/Header.css";
 import CreatedSession from "../Pages/CreateSession";
 import App from "../../App";
 import MainScreen from "../MainScreen/MainScreen.";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -11,6 +12,14 @@ export default function Header() {
         <div>
           <a href="">{"Calm | Chatbot"}</a>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+              <a href="/CreateSession">Create Session</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </Fragment>
   );
