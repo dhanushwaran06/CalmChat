bb63864e50ae9ec4a566712b107d769d263cd6fa
RyanXGregoire <ryangregoire286@gmail.com>
2024-07-08 12:54:20 -0400

    Added Styles to Main Page Mobile

---
+++ client/src/Components/Box's/Box.tsx
@@ -0,0 +1,17 @@
+import "../Styles/MainBody.css";
+// import "../Styles/MainBody.css";
+import "../Styles/Mobile.css";
+
+type Heading = {
+  head: any;
+};
+
+function Box({ head, ...Heading }): any {
+  return (
+    <div className="container">
+      <div className="box"></div>
+    </div>
+  );
+}
+
+export default Box;
--- client/src/Components/HomePage/HomePage.tsx
+++ client/src/Components/HomePage/HomePage.tsx
@@ -1,11 +1,16 @@
 import { Fragment } from "react/jsx-runtime";
+import "../Styles/MainBody.css";
+import "../Styles/Mobile.css";
 import Header from "../../NavBar/NavBar";
 
 const HomePage = () => {
   return (
     <Fragment>
       <Header />
-      <div>HomePage</div>;
+      <div>
+        <h1 className="Heading1">Would You Like to Chat with Bot</h1>
+        <h2 className="job">A Bot That Calms People from Stress</h2>
+      </div>
     </Fragment>
   );
 };
